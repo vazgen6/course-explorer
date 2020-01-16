4694c6b466beda65869bbc481a90bc0c82325824
Vazgen Sargisian <vazgen.sargisian@avetiq.de>
2020-01-17 00:14:25 +0400

    More work on Course model

--- models/Course.js
+++ models/Course.js
@@ -22,5 +22,79 @@ const CourseSchema = new mongoose.Schema({
       /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
       "Please enter a valid URL"
     ]
+  },
+  phone: {
+    type: String,
+    maxlength: [20, "Phone number cannot be longer than 20 characters"]
+  },
+  email: {
+    type: String,
+    match: /\S+@\S+\.\S+/
+  },
+  address: {
+    type: String,
+    required: [true, "Please add a valid address"]
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      require: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: "2dsphere"
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    country: String
+  },
+  careers: {
+    type: [String],
+    required: true,
+    enum: [
+      "Web Development",
+      "Mobile Development",
+      "UI/UX",
+      "Data Science",
+      "Business",
+      "Other"
+    ]
+  },
+  averageRating: {
+    type: Number,
+    min: [1, "Rating must be at least 1"],
+    max: [10, "Rating must not be more than 10"]
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: "no-photo.jpg"
+  },
+  housing: {
+    type: Boolean,
+    default: false
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
   }
 });
+
+module.exports = mongoose.model('Course', CourseSchema)
